2184899dfc9a256fd9f16cf0cb280b2233677403
William Ancizar Toro Benavides <a.toro@utp.edu.co>
2022-07-01 22:54:27 -0500

    Crear token y organizar ruta login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,33 @@
 const jwt = require("jsonwebtoken");
 const Users = require("../models/users.models");
 
+// Duración del toke
+const duracionToken = 365 * 24 * 60 * 60; 
+
+// Crear token
+const crearToken = (id) => {
+    return jwt.sign({ id }, "SECRET MESSAGE", {expiresIn: duracionToken});
+}
+
 module.exports.login = async (req, res) => {
+    // TODO Organizar el login
     try {
-        const body = req. body;
-        console.log(body);
-    } catch (error) {
+        const {username, password} = req.body;
+        //console.log("Username: " + username);
+        const user = await Users.findOne({where: { username: username }});
         
+        if(!user) return res.status(401).json({mesaage: "No autorizado"});
+
+        console.log(user.authenticate(password));
+
+        if( user.authenticate(password)) {
+            const token = crearToken(user.id);
+            return res.status(200).json({token})
+        }
+
+        res.status(401).json({mesaage: "No autorizado - "});
+    } catch (error) {
+        res.status(500).json(error.message);
     }
 };
 
@@ -14,8 +35,10 @@ module.exports.register = async (req, res) => {
     try {
         const { username, password} = req.body;
         const nuevoUsuario = await Users.create({ username, password });
-        console.log(password);
-        res.status(200).json(nuevoUsuario);
+        
+        const token = crearToken(nuevoUsuario.id);
+
+        res.status(200).json({token});
     } catch (error) {
         res.status(500).json(error.message);   
     }
