c986fe1793a4e89b487735b5d21a4fcddd39c45a
William Ancizar Toro Benavides <a.toro@utp.edu.co>
2022-07-02 22:25:13 -0500

    Agregar variable de entorno PORT

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const app = require("./app");
 const sequelize = require("./db.connection");
+require('dotenv').config();
+
 const PORT = process.env.PORT || 5000;
 //require('./models/personajes.models');
 //require('./models/genero.models');
