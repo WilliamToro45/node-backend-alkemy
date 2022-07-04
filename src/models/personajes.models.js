b8f0369b930fc028bdbc3cc0bfd51158a667ae08
William Ancizar Toro Benavides <a.toro@utp.edu.co>
2022-07-03 20:32:00 -0500

    Agregar validaciones de datos

--- src/models/personajes.models.js
+++ src/models/personajes.models.js
@@ -1,41 +1,63 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 //const Peliculas = require('./pelicula-serie.models');
-const sequelize = require('../db.connection');
-const Peliculas = require('./pelicula-serie.models');
+const sequelize = require("../db.connection");
+const Peliculas = require("./pelicula-serie.models");
 
-const Personajes =  sequelize.define("Personaje", {
+const Personajes = sequelize.define(
+  "Personaje",
+  {
     id: {
-        type: DataTypes.BIGINT,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+      autoIncrement: true,
     },
     imagen: {
-        type: DataTypes.BLOB,
-        allowNull: false 
+      type: DataTypes.BLOB,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "El campo imagen no puede estar vacío.",
+        },
+        notNull: {
+          msg: "El campo imagen no admite valores tipo null.",
+        },
+      },
     },
     nombre: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {msg: "El campo nombre no puede estar vacío."},
+        notNull: { msg: "El campo nombre no admite valores de tipo null."}
+      },
     },
     edad: {
-        type: DataTypes.INTEGER,
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: { msg: "El campo edad solo admite valores enteros."}
+      }
     },
     peso: {
-        type: DataTypes.INTEGER
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: { msg: "El campo peso solo admite valores enteros."}
+      }
     },
     historia: {
-        type: DataTypes.STRING
-    }
-},{
+      type: DataTypes.STRING,
+    },
+  },
+  {
     // Other options
     timestamps: true,
-})
+  }
+);
 
 //Personajes.hasMany(Peliculas);
 // Relaciones
 // Un personaje puede tener varias peliculas o series
 //Personajes.hasMany(Peliculas, {
-    // Especificar opciones como foreigmkey y sourceKey
+// Especificar opciones como foreigmkey y sourceKey
 //    foreignKey: 'idPersonaje',
 //    sourceKey: 'id'
 //})
