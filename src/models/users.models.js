752b7fc2ee5c605222c5c2475ba95d133d20c9de
William Ancizar Toro Benavides <a.toro@utp.edu.co>
2022-07-03 21:25:31 -0500

    Agregar validaciones a los campos

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -5,37 +5,50 @@ const useBcrypt = require('sequelize-bcrypt');
 require('dotenv').config();
 
 const Users = sequelize.define("Users", {
-    username: {
+    email: {
         type: DataTypes.STRING,
-        unique: true,
+        unique: {
+            msg: "El usuario ya se encuentra registrado."
+        },
         allowNull: false,
         required: true,
         validate: {
-            notEmpty: false,
-            async validarUsername(value){
-                const username = await Users.findOne({where: {username: value}});
-                if (username) throw new Error("El usuario ya se encuetra registrado");
+            notEmpty: {
+                msg: "El campo email no puede estar vacío",
+            },
+            isEmail:{
+                msg: "Ingrese un email válido."
             }
+            /*
+            async validarUser(value){
+                const user = await Users.findOne({where: {username: value}});
+                if (user) throw new Error("El usuario ya se encuentra registrado.");
+            }*/
         },
         set(value) {
-            this.setDataValue('username', value.toLowerCase());
+            this.setDataValue('email', value.toLowerCase());
         }
     },
     password: {
         type: DataTypes.STRING(64),
         required: true,
         allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "El campo contraseña no puede estar vacío."
+            },
+        }
         /*set(value) {
             const saltos = bcrypt.genSaltSync();
             const pwd = bcrypt.hashSync(value, saltos);
             this.setDataValue('password', pwd);
         }*/
     }
-})
+});
 
 const options = {
     field: 'password',
-    rounds: process.env.NUM_SALTOS || 12,
+    rounds: 12,
     compare: 'authenticate'
 }
 
