62c865c5e16129ab0b7021705869e68e372eb34a
William Ancizar Toro Benavides <a.toro@utp.edu.co>
2022-07-03 23:03:53 -0500

    Cambio de las rutas

--- src/routes/personajes.routes.js
+++ src/routes/personajes.routes.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const personajesController = require('../controllers/personajes.controller');
 
-router.get("/characters", personajesController.obtenerPersonajes);
-router.post("/characters", personajesController.crearPersonaje);
-router.put("/characters/:id", personajesController.actualizarPersonaje);
-router.delete("/characters/:id", personajesController.eliminarPersonaje);
-router.get("/characters/:id", personajesController.obtenerPersonaje);
+router.get("/", personajesController.obtenerPersonajes);
+router.post("/", personajesController.crearPersonaje);
+router.put("/:id", personajesController.actualizarPersonaje);
+router.delete("/:id", personajesController.eliminarPersonaje);
+router.get("/:id", personajesController.obtenerPersonaje);
 
 // Consultar detalles de personajes y películas relacionadas.
-router.get('/characters/:id/details', personajesController.obtenerDetallesPersonaje);
+router.get('/:id/details', personajesController.obtenerDetallesPersonaje);
 
 // Rutas de consulta
 //router.get('/characters?name', personajesController.busquedaPorNombre)
