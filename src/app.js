f4766ec1d788fdaa2822d6f0e7cb60e22ca72eb1
William Ancizar Toro Benavides <a.toro@utp.edu.co>
2022-07-03 23:04:30 -0500

    Agregar autenticación y carga de arhivos

--- src/app.js
+++ src/app.js
@@ -1,18 +1,32 @@
 const express = require('express');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const app = express();
 
 const routesPersonajes = require('./routes/personajes.routes');
 const routesPeliculas = require('./routes/peliculas.routes');
+const routerGeneros = require('./routes/genero.routes');
 const authRoutes = require('./routes/auth.routes');
 
-// Middleware
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+// Middleware para validar autorización.
+const requiereAuth = require('./middleware/requiereAuth');
 
+// Middleware
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(fileUpload({
+    createParentPath: true,
+    //limits: {
+        // Limitar el tamaño máximo a 2 MB.
+        //fileSize: 2 * 1024 * 1024 * 1024 
+    //}
+})) // Para cargar archivos.
 
 // Routers
-app.use(routesPersonajes);
-app.use('/movies', routesPeliculas);
+app.use('/characters', requiereAuth, routesPersonajes);
+app.use('/movies', requiereAuth, routesPeliculas);
+app.use('/genre', requiereAuth, routerGeneros);
 app.use('/auth', authRoutes);
 
 
