492f75436f42a5392e9df778312a3bf092e527f6
William Ancizar Toro Benavides <a.toro@utp.edu.co>
2022-07-03 22:57:22 -0500

    Configuración final

--- src/controllers/peliculas.controller.js
+++ src/controllers/peliculas.controller.js
@@ -1,18 +1,20 @@
+const mensajesDeError = require("../lib/validarMensajesDeErrores");
 const Peliculas = require("../models/pelicula-serie.models");
+const Personajes = require("../models/personajes.models");
 
 module.exports.obtenerPeliculas = async (req, res) => {
   try {
     // Atributes determina que atributos nos regresa la consulta
-    const peliculas = await Peliculas.findAll(
-        /*{
-      attributes: ["imagen", "titulo", "fechaCreacion"],
-    }*/
-    );
-    if (!peliculas) return res.status(404);
+    const peliculas = await Peliculas
+      .findAll({
+        attributes: ["imagen", "titulo", "fechaCreacion"],
+      });
+      
+    //if (!peliculas) return res.status(404);
 
     res.status(200).json(peliculas);
   } catch (error) {
-    res.status(500).json(error.message);
+    res.status(500).json(mensajesDeError(error.errors));
   }
 };
 
@@ -20,57 +22,94 @@ module.exports.obtenerPelicula = async (req, res) => {
   try {
     // Atributes determina que atributos nos regresa la consulta
     const { id } = req.params;
-    const pelicula = await Peliculas.findOne({ where: { id }});
-    
-    if (!pelicula) return res.status(404).json({message: "File not Found"});
+    const pelicula = await Peliculas.findOne({ where: { id } });
+
+    if (!pelicula)
+      return res
+        .status(404)
+        .json({ message: "Película o serie no encontrada" });
 
     res.status(200).json(pelicula);
   } catch (error) {
-    res.status(500).json(error.message);
+    res.status(500).json(mensajesDeError(error.errors));
   }
 };
 
 module.exports.crearPelicula = async (req, res) => {
   try {
-    const { imagen, titulo, fechaCreacion, calificacion, idPersonaje } =
-      req.body;
-    const nuevaPelicula = await Peliculas.create({
-      imagen,
-      titulo,
-      fechaCreacion,
-      calificacion,
-      idPersonaje,
-    });
-    res.status(202).json(nuevaPelicula);
+    if (req.files) {
+      const { imagen } = req.files;
+      const { titulo, fechaCreacion, calificacion, idGenero } = req.body;
+      const nuevaPelicula = await Peliculas.create({
+        imagen,
+        titulo,
+        fechaCreacion,
+        calificacion,
+        idGenero,
+      });
+      res.status(202).json(nuevaPelicula);
+    } else {
+      res
+        .status(500)
+        .json({ mensaje: "No ha seleccionado un archivo de imagen." });
+    }
   } catch (error) {
-    res.status(500).json(error.message);
+    res.status(500).json(mensajesDeError(error.errors));
   }
 };
 
 module.exports.eliminarPelicula = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const eliminarPelicula = await Peliculas.destroy({ where: { id }});
+  try {
+    const { id } = req.params;
+    const eliminarPelicula = await Peliculas.destroy({ where: { id } });
 
-        if(eliminarPelicula == 0) return res.json({message: "File not exits"});
+    if (eliminarPelicula == 0)
+      return res.json({ mensaje: "Película o serie no encontrada." });
 
-        res.status(200).json({message: "File delete"});
-    } catch (error) {
-        res.status(500).json(error);
-    }
+    res.status(200).json({ mensaje: "Película o serie eliminada con éxito." });
+  } catch (error) {
+    res.status(500).json(mensajesDeError(error.errors));
+  }
 };
 
 module.exports.actualizarPelicula = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const pelicula = await Peliculas.findOne({ where: { id }});
-        // Actualizar la tarea
-        pelicula.set(req.body);
-        const actualizarPelicula = await pelicula.save();
+  try {
+    if (req.files) {
+      const { id } = req.params;
+      const { imagen } = req.files;
+      const { titulo, fechaCreacion, calificacion, idGenero } = req.body;
 
-        res.status(202).json(actualizarPelicula);
-        
-    } catch (error) {
-        res.status(500).json(error.message);
+      const actualizarPelicula = await Peliculas.update({
+        imagen,
+        titulo,
+        fechaCreacion,
+        calificacion,
+        idGenero,
+      }, {
+        where: { id }
+      });
+
+      res.status(202).json(actualizarPelicula);
+    } else {
+      res
+        .status(500)
+        .json({ mensaje: "No ha seleccionado un archivo de imagen." });
     }
+  } catch (error) {
+    res.status(500).json(mensajesDeError(error.errors));
+  }
 };
+
+
+module.exports.obtenerDetallesPelicula = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const detallePelicula = await Peliculas.findOne({where: { id }});
+    
+    const personajes = await Personajes.findAll({where: {idPelicula: detallePelicula.id} });
+    res.status(200).json({detallePelicula, personajes})
+  } catch (error) {
+    res.status(500).json(mensajesDeError(error.errors));
+  }
+}
