2a139af4d8fb0442eda639d822d97c96c0ec3295
William Ancizar Toro Benavides <a.toro@utp.edu.co>
2022-07-03 22:57:00 -0500

    Agregar ruta detalle de peliculas y personajes

--- src/routes/peliculas.routes.js
+++ src/routes/peliculas.routes.js
@@ -8,4 +8,7 @@ router.post('/', controllerPeliculas.crearPelicula);
 router.put('/:id', controllerPeliculas.actualizarPelicula);
 router.delete('/:id', controllerPeliculas.eliminarPelicula);
 
+// Consultar detalles de peliculas y personajes relacionados.
+router.get('/:id/details', controllerPeliculas.obtenerDetallesPelicula);
+
 module.exports = router;
